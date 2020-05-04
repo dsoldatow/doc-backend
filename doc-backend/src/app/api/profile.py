44f9984528e0b0fc3016d8ae2f453733ffbfb30b
dsoldatow <dsoldatov26@gmail.com>
2020-05-04 15:24:16 +0700

    fix

--- doc-backend/src/app/api/profile.py
+++ doc-backend/src/app/api/profile.py
@@ -13,14 +13,16 @@ async def get_profile_user(db, id_user):
     resp = await db.exec_("select * from user_profiles where id_user =  $1", id_user)
     return resp
 
+
 async def get_profile_doctor(db, id_doctor):
     resp = await db.exec_("select * from doctor_profiles where id_user =  $1", id_doctor)
     return resp
 
+
 def valid(data, keys):
     answer = True
     for key in keys:
-        answer*= key in data.keys()
+        answer *= key in data.keys()
     return bool(answer)
 
 
@@ -42,11 +44,14 @@ async def handler_get_profile(request):
 
     return web.json_response(response_data[0])
 
+
 async def update_user_profile(request, data):
     pass
 
+
 async def update_doctor_profile(request, data):
     pass
+
 
 async def handler_update_profile(request):
     data = await request.post()
